7a95f782ac7c49d69bacf796935de3233b2423ec
can1357 <me@can.ac>
2022-05-25 07:37:48 +0300

    Move operator traits to lang/operator, add BC::NOP.

--- include/lang/operator.hpp
+++ include/lang/operator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <lang/types.hpp>
 #include <vm/bc.hpp>
+#include <lang/lexer.hpp>
 #include <tuple>
 
 namespace lightning::core {
@@ -11,4 +12,28 @@ namespace lightning::core {
 	//
 	std::pair<any, bool> apply_unary(vm* L, any a, bc::opcode op);
 	std::pair<any, bool> apply_binary(vm* L, any a, any b, bc::opcode op);
+
+	// List of operators with their traits.
+	//
+	struct operator_traits {
+		// Lexer token and the emitted bytecode.
+		//
+		lex::token token;
+		bc::opcode opcode;
+
+		// Precedence.
+		//
+		uint8_t prio_left;
+		uint8_t prio_right;
+	};
+	static constexpr operator_traits unary_operators[] = {
+		 {lex::token_lnot, bc::LNOT, 3, 2},
+		 {lex::token_sub, bc::ANEG, 3, 2},
+		 {lex::token_add, bc::NOP, 0, 0},
+	};
+	static constexpr operator_traits binary_operators[] = {
+		 {lex::token_add, bc::AADD, 6, 6}, {lex::token_sub, bc::ASUB, 6, 6},    {lex::token_mul, bc::AMUL, 5, 5},  {lex::token_div, bc::ADIV, 5, 5}, {lex::token_mod, bc::AMOD, 5, 5},
+		 {lex::token_pow, bc::APOW, 5, 5}, {lex::token_land, bc::LAND, 14, 14}, {lex::token_lor, bc::LOR, 15, 15}, {lex::token_cat, bc::SCAT, 5, 5}, {lex::token_eq, bc::CEQ, 10, 10},
+		 {lex::token_ne, bc::CNE, 10, 10}, {lex::token_lt, bc::CLT, 9, 9},      {lex::token_gt, bc::CGT, 9, 9},    {lex::token_le, bc::CLE, 9, 9},   {lex::token_ge, bc::CGE, 9, 9},
+	};
 };

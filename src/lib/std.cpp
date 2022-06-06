ca84d4928970d25e22fba04671262bbedd82cdd0
can1357 <me@can.ac>
2022-06-06 08:55:07 +0300

    Add chrono API.

--- src/lib/std.cpp
+++ src/lib/std.cpp
@@ -5,6 +5,7 @@
 #include <lang/parser.hpp>
 #include <cmath>
 #include <bit>
+#include <chrono>
 
 namespace li::lib {
 #define OPTIONAL_SELF() \
@@ -154,6 +155,24 @@ namespace li::lib {
 
 			bool res           = L->scall(n - 2, f);
 			L->stack[apos - 1] = res;
+			return true;
+		});
+
+		// Time.
+		//
+		util::export_as(L, "chrono.now", [](vm* L, any* args, slot_t n) {
+			auto time = std::chrono::high_resolution_clock::now().time_since_epoch();
+			L->push_stack(number(time / std::chrono::duration<double, std::milli>(1)));
+			return true;
+		});
+		util::export_as(L, "chrono.cycles", [](vm* L, any* args, slot_t n) {
+			uint64_t cycles = 0;
+#if __has_builtin(__builtin_readcyclecounter)
+			cycles = __builtin_readcyclecounter();
+#elif LI_ARCH_X86 && LI_MSVC
+			cycles = __rdtsc();
+#endif
+			L->push_stack(number(cycles));
 			return true;
 		});
 

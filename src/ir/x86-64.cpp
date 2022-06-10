226e2818a1a129a6fc27691c895514bead6912dd
can1357 <me@can.ac>
2022-06-10 15:58:49 +0300

    Generate VXORPS/VPCMPEQB for special constants summoned with RM(x).

--- src/ir/x86-64.cpp
+++ src/ir/x86-64.cpp
@@ -42,15 +42,75 @@ namespace li::ir {
 	#undef ENUMERATOR
 	#undef ENUM_FLAGS
 
+	// Instructions.
+	//
+	enum encoding_directive : msize_t {
+		ENC_W_R,
+		ENC_RW_R,
+		ENC_RW,
+		ENC_W_R_R,
+		ENC_W_N_R_R,
+		ENC_F_R_R,
+	};
+
+	#define INSN_W_R(name, ...) \
+		static void name(mblock& blk, mreg a, mop b) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__.rsvd = ENC_W_R}, a, b}); }
+	#define INSN_RW_R(name, ...) \
+		static void name(mblock& blk, mreg a, mop b) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_RW_R}, a, a, b}); }
+	#define INSN_RW(name, ...) \
+		static void name(mblock& blk, mreg a) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_RW}, a, a}); }
+	#define INSN_W_R_R(name, ...) \
+		static void name(mblock& blk, mreg a, mop b, mop c) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_W_R_R}, a, b, c}); }
+	#define INSN_W_N_R_R(name, ...) \
+		static void name(mblock& blk, mreg a, mop b, mop c) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_W_N_R_R}, a, b, c}); }
+	#define INSN_F_R_R(name, ...) \
+		static void name(mblock& blk, flag_id flag, mreg a, mop b) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd=ENC_F_R_R}, flag, a, b}); }
+
+	INSN_RW(NEG);
+	INSN_RW(NOT, .trashes_flags = false, );
+	INSN_RW_R(SHR);
+	INSN_RW_R(SHL);
+	INSN_RW_R(ADD);
+	INSN_RW_R(SUB);
+	INSN_RW_R(OR);
+	INSN_RW_R(AND);
+	INSN_RW_R(IMUL);
+	INSN_RW_R(XOR);
+	INSN_RW_R(CMOVZ);
+	INSN_RW_R(CMOVNBE);
+	INSN_RW_R(CRC32); // Always qword.
+	INSN_W_R(LEA, .trashes_flags = false, );
+	INSN_W_R_R(BZHI, .trashes_flags = false, );
+	INSN_W_R_R(ROUNDSD, .trashes_flags = false, );
+	INSN_W_N_R_R(VROUNDSD, .trashes_flags = false, );
+	INSN_RW_R(DIVSD, .trashes_flags = false, );
+	INSN_RW_R(MULSD, .trashes_flags = false, );
+	INSN_RW_R(ADDSD, .trashes_flags = false, );
+	INSN_RW_R(SUBSD, .trashes_flags = false, );
+	INSN_RW_R(XORPS, .trashes_flags = false, );
+	INSN_W_R_R(VXORPS, .trashes_flags = false, );
+	INSN_RW_R(PCMPEQB, .trashes_flags = false, );
+	INSN_W_R_R(VPCMPEQB, .trashes_flags = false, );
+	INSN_W_R_R(VDIVSD, .trashes_flags = false, );
+	INSN_W_R_R(VMULSD, .trashes_flags = false, );
+	INSN_W_R_R(VADDSD, .trashes_flags = false, );
+	INSN_W_R_R(VSUBSD, .trashes_flags = false, );
+	INSN_F_R_R(CMP);
+	INSN_F_R_R(TEST);
+	INSN_F_R_R(PTEST);
+	INSN_F_R_R(VPTEST);
+	INSN_F_R_R(VUCOMISD);
+	INSN_F_R_R(UCOMISD);
+
 	// Operand helpers.
 	//
 	#define RI(x)    get_ri_for(b, x, false)
-	#define RIi(x)    get_ri_for(b, x, true)
-	#define RM(x)  get_rm_for(b, x)
-	#define REG(x) get_reg_for(b, x->as<insn>())
+	#define RIi(x)   get_ri_for(b, x, true)
+	#define RM(x)    get_rm_for(b, x)
+	#define REG(x)   get_reg_for(b, x->as<insn>())
 	#define YIELD(x) yield_value(b, i, x)
 
-	#if VM_AS_CONST
+	#if !LI_DEBUG
 		#define REF_VM() mop(intptr_t(b->source->L))
 	#else
 		#define REF_VM() mop(mreg(vreg_vm))
@@ -103,69 +163,31 @@ namespace li::ir {
 	}
 	static mop get_rm_for(mblock& b, value* i) {
 		if (i->is<constant>()) {
+			if (auto c = i->as<constant>(); c->is(type::f64)) {
+				auto dst = b->next_fp();
+				if (c->i == 0) {
+					if constexpr (USE_AVX) {
+						VXORPS(b, dst, dst, dst);
+						return dst;
+					} else {
+						XORPS(b, dst, dst);
+						return dst;
+					}
+				} else if (c->i == -1ll) {
+					if constexpr (USE_AVX) {
+						VPCMPEQB(b, dst, dst, dst);
+						return dst;
+					} else {
+						PCMPEQB(b, dst, dst);
+						return dst;
+					}
+				}
+			}
 			return b->add_const(i->as<constant>()->to_any());
 		} else {
 			return get_reg_for(b, i->as<insn>());
 		}
 	}
-
-	// Instructions.
-	//
-	enum encoding_directive : msize_t {
-		ENC_W_R,
-		ENC_RW_R,
-		ENC_RW,
-		ENC_W_R_R,
-		ENC_W_N_R_R,
-		ENC_F_R_R,
-	};
-
-	#define INSN_W_R(name, ...) \
-		static void name(mblock& blk, mreg a, mop b) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__.rsvd = ENC_W_R}, a, b}); }
-	#define INSN_RW_R(name, ...) \
-		static void name(mblock& blk, mreg a, mop b) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_RW_R}, a, a, b}); }
-	#define INSN_RW(name, ...) \
-		static void name(mblock& blk, mreg a) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_RW}, a, a}); }
-	#define INSN_W_R_R(name, ...) \
-		static void name(mblock& blk, mreg a, mop b, mop c) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_W_R_R}, a, b, c}); }
-	#define INSN_W_N_R_R(name, ...) \
-		static void name(mblock& blk, mreg a, mop b, mop c) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd = ENC_W_N_R_R}, a, b, c}); }
-	#define INSN_F_R_R(name, ...) \
-		static void name(mblock& blk, flag_id flag, mreg a, mop b) { blk.instructions.push_back(minsn{LI_STRCAT(ZYDIS_MNEMONIC_, name), {__VA_ARGS__ .rsvd=ENC_F_R_R}, flag, a, b}); }
-
-	INSN_RW(NEG);
-	INSN_RW(NOT, .trashes_flags = false, );
-	INSN_RW_R(SHR);
-	INSN_RW_R(SHL);
-	INSN_RW_R(ADD);
-	INSN_RW_R(SUB);
-	INSN_RW_R(OR);
-	INSN_RW_R(AND);
-	INSN_RW_R(IMUL);
-	INSN_RW_R(XOR);
-	INSN_RW_R(CMOVZ);
-	INSN_RW_R(CMOVNBE);
-	INSN_RW_R(CRC32); // Always qword.
-	INSN_W_R(LEA, .trashes_flags = false, );
-	INSN_W_R_R(BZHI, .trashes_flags = false, );
-	INSN_W_R_R(ROUNDSD, .trashes_flags = false, );
-	INSN_W_N_R_R(VROUNDSD, .trashes_flags = false, );
-	INSN_RW_R(DIVSD, .trashes_flags = false, );
-	INSN_RW_R(MULSD, .trashes_flags = false, );
-	INSN_RW_R(ADDSD, .trashes_flags = false, );
-	INSN_RW_R(SUBSD, .trashes_flags = false, );
-	INSN_RW_R(XORPS, .trashes_flags = false, );
-	INSN_W_R_R(VXORPS, .trashes_flags = false, );
-	INSN_W_R_R(VDIVSD, .trashes_flags = false, );
-	INSN_W_R_R(VMULSD, .trashes_flags = false, );
-	INSN_W_R_R(VADDSD, .trashes_flags = false, );
-	INSN_W_R_R(VSUBSD, .trashes_flags = false, );
-	INSN_F_R_R(CMP);
-	INSN_F_R_R(TEST);
-	INSN_F_R_R(PTEST);
-	INSN_F_R_R(VPTEST);
-	INSN_F_R_R(VUCOMISD);
-	INSN_F_R_R(UCOMISD);
 
 	// Emits a type check of the temporary given into a flag and sets the condition flag on the temporary.
 	//
